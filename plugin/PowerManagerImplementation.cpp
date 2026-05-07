b825c69dfa41b6f66e9f4452cf7e76b78d41d215
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-05-07 17:25:12 +0530

    Update PowerManagerImplementation.cpp

--- plugin/PowerManagerImplementation.cpp
+++ plugin/PowerManagerImplementation.cpp
@@ -372,7 +372,7 @@ namespace Plugin {
         if (currState != newState) {
             char telemetryPwrChange[64];
             snprintf(telemetryPwrChange, sizeof(telemetryPwrChange), "Power Mode Change from %s to %s", util::str(currState), util::str(newState));
-            t2_event_s((char*)"SYST_INFO_POWER_CHANGE", telemetryPwrChange);
+            t2_event_d((char*)"SYST_INFO_POWER_CHANGE", 1);
 
             // Check if sync state change required
             isSync = isSyncStateChange(currState, newState);
@@ -386,7 +386,7 @@ namespace Plugin {
                     char telemetryMsg[128];
                     snprintf(telemetryMsg, sizeof(telemetryMsg), "Ignore Power Mode Change to %s as device is in transient deep sleep state, elapsed: %" PRId64 " sec",
                              util::str(newState), std::chrono::duration_cast<std::chrono::seconds>(_deepSleepController.Elapsed()).count());
-                    t2_event_s((char*)"SYST_ERR_SetPwrStateFail", telemetryMsg);
+                    t2_event_d((char*)"SYST_ERR_SetPwrStateFail", 1);
  
 
                     selfLock.Unlock();
@@ -668,7 +668,7 @@ namespace Plugin {
         LOGINFO(">> nwStandbyMode: %s", (standbyMode ? "enabled" : "disabled"));
         char telemetryMsg[64];
         snprintf(telemetryMsg, sizeof(telemetryMsg), "Set Network Standby Mode: %s", (standbyMode ? "enabled" : "disabled"));
-        t2_event_s((char*)"SYS_INFO_STANDBYMODE", telemetryMsg);
+        t2_event_d((char*)"SYS_INFO_STANDBYMODE", 1);
 
         _apiLock.Lock();
 
