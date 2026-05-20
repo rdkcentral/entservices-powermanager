23da2b2284e1d68168fb18fe855a843e5eaa9266
smohap466_comcast <srinibas_mohapatra@comcast.com>
2026-05-20 11:29:36 +0530

    Updated the Invalid Markers

--- plugin/PowerManagerImplementation.cpp
+++ plugin/PowerManagerImplementation.cpp
@@ -370,8 +370,6 @@ namespace Plugin {
 
         // Process request only if requested state is not same as current state
         if (currState != newState) {
-            char telemetryPwrChange[64];
-            snprintf(telemetryPwrChange, sizeof(telemetryPwrChange), "Power Mode Change from %s to %s", util::str(currState), util::str(newState));
             t2_event_d((char*)"SYST_INFO_POWER_CHANGE", 1);
 
             // Check if sync state change required
@@ -383,9 +381,6 @@ namespace Plugin {
 
                     LOGINFO("deepsleep in  progress  ignoring %s request, elapsed: %" PRId64 " sec",
                             util::str(newState), std::chrono::duration_cast<std::chrono::seconds>(_deepSleepController.Elapsed()).count());
-                    char telemetryMsg[128];
-                    snprintf(telemetryMsg, sizeof(telemetryMsg), "Ignore Power Mode Change to %s as device is in transient deep sleep state, elapsed: %" PRId64 " sec",
-                             util::str(newState), std::chrono::duration_cast<std::chrono::seconds>(_deepSleepController.Elapsed()).count());
                     t2_event_d((char*)"SYST_ERR_SetPwrStateFail", 1);
  
 
@@ -666,8 +661,6 @@ namespace Plugin {
     Core::hresult PowerManagerImplementation::SetNetworkStandbyMode(const bool standbyMode)
     {
         LOGINFO(">> nwStandbyMode: %s", (standbyMode ? "enabled" : "disabled"));
-        char telemetryMsg[64];
-        snprintf(telemetryMsg, sizeof(telemetryMsg), "Set Network Standby Mode: %s", (standbyMode ? "enabled" : "disabled"));
         t2_event_d((char*)"SYS_INFO_STANDBYMODE", 1);
 
         _apiLock.Lock();
