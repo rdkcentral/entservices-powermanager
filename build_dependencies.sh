ddd9e3692987f42197bddea95d7aa7263a6dcdde
Yuvaramachandran Gurusamy <yuvaramachandran_gurusamy@comcast.com>
2026-06-11 01:26:24 +0530

    RDKEMW-19860: Fix for unit test and native build failures
    Signed-off-by: Yuvaramachandran Gurusamy <yuvaramachandran_gurusamy@comcast.com>

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -29,7 +29,7 @@ cd ..
 
 git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
 
-git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+git clone --branch R4.4.3 https://github.com/rdkcentral/Thunder.git
 
 git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 
